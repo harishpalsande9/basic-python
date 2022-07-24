d6f57349dcddbff2db5eedf729bbd2447e6c161c
harishpalsande9 <harishpalsande9>
2022-07-24 12:30:34 +0530

    Python Data Type

--- function/test1.py
+++ function/test1.py
@@ -1,1 +1,18 @@
 print("Function in Python")
+
+def add1():
+    print("sum with by hard code values")
+    x = 10
+    y = 20
+    c = x + y
+    print(c)
+    
+def add2():
+    print("Sum With Input values")
+    x = int(input("Enter First Number : "))
+    y = int(input("Enter Second Number : "))
+    c = x + y
+    print("The Answer Is : ",c)
+    
+add1()   
+add2()
