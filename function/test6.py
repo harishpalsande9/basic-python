9c451ecd49994b8a831c6bb63d8ca963858c431a
harishpalsande9 <harishpalsande9@gmail.com>
2022-08-01 17:05:09 +0530

    Positional   Argument

---
+++ function/test6.py
@@ -0,0 +1,18 @@
+#Positional   Argument 
+
+def pw(x,y):
+    z = x**y 
+    print(z)
+    
+pw(2,5)
+
+
+
+print()
+
+def show(name , surname):
+    print(name + surname)
+
+
+show(name = "Harish" , surname="Palsande")
+
