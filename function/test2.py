5c6a8efaa9ceed287c6cb551c5321741ecf8f63b
harishpalsande9 <harishpalsande9@gmail.com>
2022-08-01 09:49:49 +0530

    Test

--- function/test2.py
+++ function/test2.py
@@ -1,1 +1,28 @@
-s
+#Return Statement Single Value 
+#Defining a Function
+
+def add():
+    x = 10
+    y = 20
+    c = x + y 
+    return c
+    
+add()
+
+sum = add()
+print(sum)
+
+
+def add1(x , y ):
+    return x + y
+    
+sum1 = add1( 10 , 10 )
+print(sum1)
+
+
+def sub():
+    a = int(input("Enter First"))
+    b = int(input("Enter second"))
+    return a  - b
+    
+print(sub())
